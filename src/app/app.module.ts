a2dadb1670289c248859c61065d06af65bef2d3f
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2019-03-27 22:35:40 +0530

    Implemented services and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
   ],
+
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
+
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
